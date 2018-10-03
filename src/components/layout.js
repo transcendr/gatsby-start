fb00a364e3877a4c222c76d51b616bc8d229cb45
Bryan McConnahea <bryan@iocharged.com>
2018-10-03 14:17:33 +0200

    implements page transition plugin

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,44 +5,47 @@ import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
 import './layout.css'
+import PageTransition from 'gatsby-plugin-page-transitions';
 
 const Layout = ({ children, node = {} }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+  <PageTransition>
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
           }
         }
-      }
-    `}
-    render={data => (
-      <>
-        <Helmet
-          title={node.title || data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-            { name: 'google-site-verification', content: 'gvgaIA35NAU5AEzzGiKVqZg3FBlVOdIBz_jKAS-BLh4' }
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-        <Header siteTitle={node.title || data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
-      </>
-    )}
-  />
+      `}
+      render={data => (
+        <>
+          <Helmet
+            title={node.title || data.site.siteMetadata.title}
+            meta={[
+              { name: 'description', content: 'Sample' },
+              { name: 'keywords', content: 'sample, something' },
+              { name: 'google-site-verification', content: 'gvgaIA35NAU5AEzzGiKVqZg3FBlVOdIBz_jKAS-BLh4' }
+            ]}
+          >
+            <html lang="en" />
+          </Helmet>
+          <Header siteTitle={node.title || data.site.siteMetadata.title} />
+          <div
+            style={{
+              margin: '0 auto',
+              maxWidth: 960,
+              padding: '0px 1.0875rem 1.45rem',
+              paddingTop: 0,
+            }}
+          >
+            {children}
+          </div>
+        </>
+      )}
+    />
+  </PageTransition>
 )
 
 Layout.propTypes = {
